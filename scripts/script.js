a15769530094c95fb31b062a302826c12b2eb423
Oli Stocks <stocksolly@gmail.com>
2024-06-23 14:49:11 +0100

    Corrected combined scrolling action with anonymous function

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,15 @@ const clickNext = document.querySelectorAll(".next");
 const scrollWindows = document.querySelectorAll(".blog-window");
 const clickPrev = document.querySelectorAll(".prev");
 
-function scrollToNext(){
-    scrollWindows[0].scrollLeft =+ previewWidth;
+function scrollToNext(index){
+    scrollWindows[index].scrollLeft += previewWidth;
 }
 
-function scrollToPrev(){
-    scrollWindows[0].scrollLeft =- previewWidth;
+function scrollToPrev(index){
+    scrollWindows[index].scrollLeft -= previewWidth;
 }
 
 for (let i = 0; i < scrollWindows.length; i++){
-    clickNext[i].addEventListener("click", scrollToNext);
-    clickPrev[i].addEventListener("click", scrollToPrev);
+    clickNext[i].addEventListener("click", () => scrollToNext(i));
+    clickPrev[i].addEventListener("click", () => scrollToPrev(i));
 }
